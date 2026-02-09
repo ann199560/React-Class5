b3b3ce167e45fc80bb74e1d08715e8b2a91d241c
Der Yu Tu <ann199560@gmail.com>
2026-02-09 14:57:52 +0800

    更改 li

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import App from "./App2.jsx";
